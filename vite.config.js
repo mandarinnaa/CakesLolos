f01bd054889edc413d6929c170f6b48617b9e4a6
michelle <mandarinaaaaa19@gmail.com>
2025-08-06 16:26:31 -0600

    Version 13

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,21 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
- base: '/build/',
-     plugins: [
+    base: '/build/',
+    plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/css/admin.css',
-                'resources/css/editor.css',
-                'resources/js/app.js'
+                'resources/css/app.css',    // Estilos generales
+                'resources/css/admin.css',  // Solo admin
+                'resources/css/editor.css', // Solo editor
+                'resources/js/app.js'       // JavaScript
             ],
-                        refresh: true,
+            refresh: true,
         })
     ],
-      build: {
+    build: {
         manifest: true,
         outDir: 'public/build',
-        emptyOutDir: true,
+        emptyOutDir: false,  // ¡IMPORTANTE! Evita que se borren otros assets
     }
-
 });
