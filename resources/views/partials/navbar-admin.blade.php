13644d587c7296c561c8e36da6af1cf5393911cb
michelle <mandarinaaaaa19@gmail.com>
2025-08-07 17:00:32 -0600

    Nav

--- resources/views/partials/navbar-admin.blade.php
+++ resources/views/partials/navbar-admin.blade.php
@@ -1,6 +1,7 @@
 @if(Auth::check() && (Auth::user()->role === 'admin' || Auth::user()->role === 'editor'))
-<head>
-<style>
+
+<nav class="navbar-admin">
+    <style>
     :root {
     --nucita-cream: #FFDE7D;
     --nucita-blush: #FFDE7D;
@@ -975,16 +976,7 @@ textarea.nucita-form-control {
     }
 }
 </style>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Cookie&display=swap" rel="stylesheet">
-
-</head>
-@endif
-
-<nav class="navbar-admin">
+    
     <div class="navbar-admin-container">
         <div class="navbar-admin-brand">
             <a href="{{ route('admin.dashboard') }}">
@@ -1055,8 +1047,7 @@ textarea.nucita-form-control {
             </div>
         </div>
     </div>
-</nav>
-<script>
+        <script>
     document.addEventListener('DOMContentLoaded', function() {
     const toggleButton = document.getElementById('navbarAdminToggle');
     const navbarCollapse = document.getElementById('navbarAdminCollapse');
@@ -1096,3 +1087,6 @@ textarea.nucita-form-control {
     }
 });
 </script>
+@endif
+
+
