61317f8fef4541e535edfdad40beb5787ff0363c
michelle <mandarinaaaaa19@gmail.com>
2025-08-07 13:28:17 -0600

    Añadir assets de Vite compilados

--- resources/views/partials/navbar-editor.blade.php
+++ resources/views/partials/navbar-editor.blade.php
@@ -689,6 +689,12 @@ body {
     }
 }
     </style>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Cookie&display=swap" rel="stylesheet">
+
 </head>
 <nav class="navbar-admin">
     <div class="navbar-admin-container">
