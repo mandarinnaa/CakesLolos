3db9f91b0ba9c89216f07b5840c3b9198cfd06cc
michelle <mandarinaaaaa19@gmail.com>
2025-08-07 12:31:24 -0600

    Version 15.4

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ export default defineConfig({
     base: '/build/', // ¡Clave para producción!
     plugins: [
         laravel({
-            base: '/build/', 
             input: [
                 'resources/css/app.css',
                 'resources/css/admin.css',
