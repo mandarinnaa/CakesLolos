76bf6ac990c8ce5692fe0d5ee84ce58f8a7b45b6
michelle <mandarinaaaaa19@gmail.com>
2025-08-09 14:10:38 -0600

    Mejoramiento 1.5

---
+++ vite.config.js
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+
+export default defineConfig({
+    base: '/build/', // ¡Clave para producción!
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/css/admin.css',
+                'resources/css/editor.css',
+                'resources/js/app.js'
+            ],
+            refresh: [
+                'resources/views/**',
+                'app/Http/Controllers/**'
+            ]
+        })
+    ],
+    build: {
+        outDir: 'public/build',
+        emptyOutDir: true,
+        manifest: true,
+        rollupOptions: {
+            output: {
+                assetFileNames: (assetInfo) => {
+                    const extType = assetInfo.name.split('.').at(1);
+                    if (extType === 'css') {
+                        return 'assets/css/[name].[hash][extname]';
+                    }
+                    return 'assets/[name].[hash][extname]';
+                }
+            }
+        }
+    }
+});
